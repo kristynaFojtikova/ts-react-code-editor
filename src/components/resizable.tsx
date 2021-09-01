82acd59653f2c35b1ed8386c15b195ba8d88f63d
Kristyna Fojtikova <fojtik.kristyna@gmail.com>
2021-09-02 01:51:28 +0200

    Resizable modifications

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import "./resizable.css";
 
@@ -7,20 +8,47 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
+  useEffect(() => {
+    let timer: any;
+    const listener = () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75);
+        }
+      }, 100);
+    };
+    window.addEventListener("resize", listener);
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, [width]);
+
   const verticalProps: ResizableBoxProps = {
     height: 300,
     width: Infinity,
     resizeHandles: ["s"],
-    maxConstraints: [Infinity, window.innerHeight * 0.9],
+    maxConstraints: [Infinity, innerHeight * 0.9],
     minConstraints: [Infinity, 50],
   };
   const horizontalProps: ResizableBoxProps = {
     height: Infinity,
-    width: window.innerWidth * 0.75,
+    width: width,
     resizeHandles: ["e"],
-    maxConstraints: [window.innerWidth * 0.9, Infinity],
-    minConstraints: [window.innerWidth * 0.2, Infinity],
+    maxConstraints: [innerWidth * 0.9, Infinity],
+    minConstraints: [innerWidth * 0.2, Infinity],
     className: "resize-horizontal",
+    onResizeStop: (event, data) => {
+      setWidth(data.size.width);
+    },
   };
 
   const props = direction === "horizontal" ? horizontalProps : verticalProps;
