e7fd56fa56aac798615713eb4ecd6e858db391ed
Kristyna Fojtikova <fojtik.kristyna@gmail.com>
2021-09-01 10:52:50 +0200

    Display user input through iframe message

--- src/index.tsx
+++ src/index.tsx
@@ -6,29 +6,11 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
-  const ref = useRef<any>();
-
-  const onSubmit = async () => {
-    if (!ref.current) {
-      return;
-    }
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
-  };
+  const esbuildService = useRef<any>();
+  const iframe = useRef<any>();
 
   const startService = async () => {
-    ref.current = await esbuild.startService({
+    esbuildService.current = await esbuild.startService({
       worker: true,
       wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
@@ -38,11 +20,43 @@ const App = () => {
     startService();
   }, []);
 
+  const onClick = async () => {
+    if (!esbuildService.current) {
+      return;
+    }
+    iframe.current.srcdoc = html;
+    const result = await esbuildService.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+  };
+
   const html = `
-  <script>
-  ${code}
-  </script>
-  `;
+    <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                try {
+                    eval(event.data);
+                } catch (err) {
+                    const root = document.querySelector('#root');
+                    root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+                    console.error(err)
+                }
+            }, false);
+            </script>
+        </body>
+    </html>
+    `;
 
   return (
     <div>
@@ -51,10 +65,14 @@ const App = () => {
         onChange={(e) => setInput(e.target.value)}
       ></textarea>
       <div>
-        <button onClick={onSubmit}>Submit!</button>
+        <button onClick={onClick}>Submit!</button>
       </div>
-      <pre>{code}</pre>
-      <iframe sandbox="allow-scripts" srcDoc={html} title="Preview" />
+      <iframe
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+        title="Code Preview"
+      />
     </div>
   );
 };
